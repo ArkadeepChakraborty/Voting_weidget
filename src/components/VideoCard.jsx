c62a78b2336c9da8c32408e0a7829286123615dd
ArkadeepChakraborty <157718373+ArkadeepChakraborty@users.noreply.github.com>
2026-03-16 20:35:20 +0530

    update videocard
    Video card updated

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,75 @@
+// import { FaPlay } from "react-icons/fa";
+
+// const VideoCard = ({ video }) => {
+
+//   const openVideo = () => {
+//     window.open(video.videoUrl, "_blank");
+//   };
+
+//   return (
+//     <div
+//       onClick={openVideo}
+//       className="cursor-pointer min-w-65 sm:min-w-70 md:min-w-75 lg:min-w-[320px] 
+//       bg-cyan-100 rounded-xl shadow-md overflow-hidden flex flex-col hover:shadow-xl transition"
+//     >
+
+//       {/* Header */}
+//       <div className="flex items-center gap-2 p-3">
+//         <img
+//           src={video.logo}
+//           alt="logo"
+//           className="w-7 h-7 rounded-full object-cover"
+//         />
+
+//         <div className="text-xs leading-tight">
+//           <p className="font-semibold text-gray-800 mb-0.5">{video.page}</p>
+//           <p className="text-gray-500 text-[10px] mb-0.5">{video.partyname}</p>
+//           <p className="text-gray-500 text-[10px]"><span className="mr-1">বিধানসভা:</span>{video.bidhabsova}</p>
+//         </div>
+//       </div>
+
+//       {/* Thumbnail */}
+//       <div className="relative px-3">
+//         <img
+//           src={video.thumbnail}
+//           alt="video"
+//           className="w-full h-36 object-cover rounded-md"
+//         />
+
+//         <div className="absolute inset-0 flex items-center justify-center">
+//           <div className="bg-red-600 p-3 rounded-full text-white shadow-lg">
+//             <FaPlay size={14} />
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* Title */}
+//       <div className="p-3">
+//         <p className="text-sm font-medium text-gray-800 leading-snug line-clamp-3">
+//           {video.title}
+//         </p>
+//         <p className="text-sm font-medium text-gray-800 leading-snug line-clamp-3">
+//           <span className="mr-1">Total Votes:</span>{video.totalVotes}
+//         </p>
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default VideoCard;
+
+
+
+
 import { FaPlay } from "react-icons/fa";
+import { useState } from "react";
 
 const VideoCard = ({ video }) => {
+
+  const [hover, setHover] = useState(false);
+
+  const videoId = video.videoUrl.split("/").pop();
 
   const openVideo = () => {
     window.open(video.videoUrl, "_blank");
@@ -9,7 +78,9 @@ const VideoCard = ({ video }) => {
   return (
     <div
       onClick={openVideo}
-      className="cursor-pointer min-w-65 sm:min-w-70 md:min-w-75 lg:min-w-[320px] 
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className="cursor-pointer min-w-65 sm:min-w-70 md:min-w-75 lg:min-w-[320px]
       bg-cyan-100 rounded-xl shadow-md overflow-hidden flex flex-col hover:shadow-xl transition"
     >
 
@@ -24,23 +95,37 @@ const VideoCard = ({ video }) => {
         <div className="text-xs leading-tight">
           <p className="font-semibold text-gray-800 mb-0.5">{video.page}</p>
           <p className="text-gray-500 text-[10px] mb-0.5">{video.partyname}</p>
-          <p className="text-gray-500 text-[10px]"><span className="mr-1">বিধানসভা:</span>{video.bidhabsova}</p>
+          <p className="text-gray-500 text-[10px]">
+            <span className="mr-1">বিধানসভা:</span>{video.bidhabsova}
+          </p>
         </div>
       </div>
 
-      {/* Thumbnail */}
+      {/* Video Area */}
       <div className="relative px-3">
-        <img
-          src={video.thumbnail}
-          alt="video"
-          className="w-full h-36 object-cover rounded-md"
-        />
 
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="bg-red-600 p-3 rounded-full text-white shadow-lg">
-            <FaPlay size={14} />
+        {!hover ? (
+          <img
+            src={video.thumbnail}
+            alt="video"
+            className="w-full h-36 object-cover rounded-md"
+          />
+        ) : (
+          <iframe
+            className="w-full h-36 rounded-md"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0`}
+            allow="autoplay"
+          />
+        )}
+
+        {!hover && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="bg-red-600 p-3 rounded-full text-white shadow-lg">
+              <FaPlay size={14} />
+            </div>
           </div>
-        </div>
+        )}
+
       </div>
 
       {/* Title */}
@@ -48,6 +133,7 @@ const VideoCard = ({ video }) => {
         <p className="text-sm font-medium text-gray-800 leading-snug line-clamp-3">
           {video.title}
         </p>
+
         <p className="text-sm font-medium text-gray-800 leading-snug line-clamp-3">
           <span className="mr-1">Total Votes:</span>{video.totalVotes}
         </p>
