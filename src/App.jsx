4f96c4508ab2412e0bc3eccf2f56eabfc2750633
aaligerd <amerpoth@gmail.com>
2026-03-17 23:35:02 +0530

    removing extra routes in production

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-// import Dashboard from "./pages/Dashboard";
-
-// function App() {
-//   return <Dashboard />;
-// }
-
-// export default App;
-
-
-
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import AdminEditor from "./pages/AdminEditor";
-import Tabs from "./pages/Tabs";
 import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/tabs" element={<Tabs/>} />
-      <Route path="/dboard" element={<Dashboard />} />
-      <Route path="/admin" element={<AdminEditor />} />
-      <Route path="/" element={<HomePage/>} />
-    </Routes>
-  );
+  return <HomePage />;
 }
 
 export default App;
